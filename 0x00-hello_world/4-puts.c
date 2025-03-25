6a110417b1b7abc71a2efd5e863615261b9de666
Dunne G Macharia <dunemacharia01@gmail.com>
2025-03-25 23:55:51 +0300

    hot fix hello, world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-/*
+/**
 * main - Entry point
 *
 * Description - Writes a program to produce a string of words
@@ -7,6 +7,6 @@
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
